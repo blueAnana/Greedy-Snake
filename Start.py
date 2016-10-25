1a486698ad9443532f26866de758148751a1b31d
blueAnana <915935284@qq.com>
2016-10-26 00:02:41 +0800

    implemented by doubly linked list

--- Start.py
+++ Start.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 import random
+import threading
 
 WIDTH = 500
 HEIGHT = 500
@@ -8,15 +9,24 @@ BOUND_Y = 10
 
 
 class Snake:
-    def __init__(self, canvas, x, y):
-        self._canvas = canvas
-        self._x = x
-        self._y = y
-        self._node = canvas.create_rectangle(BOUND_X + self._x, BOUND_Y + self._y, BOUND_X + self._x + 10, BOUND_Y + self._y + 10, fill="#000", tag="node")
+    def __init__(self, canvas_, x_, y_):
+        self.canvas = canvas_
+        self.x = x_
+        self.y = y_
+        self.next = None
+        self.prev = None
+        self.node = self.canvas.create_rectangle(BOUND_X + self.x, BOUND_Y + self.y, BOUND_X + self.x + 10, BOUND_Y + self.y + 10, fill="#000", tag="node")
 
     def snakeMove(self, speed, toX , toY):
-        self._canvas.move(self._node, toX, toY)
-        self._canvas.after(speed, self.snakeMove, speed, toX, toY)
+        self.canvas.move(self.node, toX, toY)
+        self.canvas.after(speed, self.snakeMove, speed, toX, toY)
+
+    def moveTo(self, toX, toY):
+        self.x = toX
+        self.y = toY
+        self.canvas.delete(self.node)
+        self.node = self.canvas.create_rectangle(BOUND_X + self.x, BOUND_Y + self.y, BOUND_X + self.x + 10, BOUND_Y + self.y + 10, fill="#000", tag="node")
+
 
 
 class Start:
@@ -38,16 +48,39 @@ class Start:
         self._canvas.create_rectangle(10, 10, 490, 490, fill='#fff')
 
         # initial snake with 3 nodes
-        head = Snake(self._canvas, 20, 0)
-        node2 = Snake(self._canvas, 10, 0)
-        node3 = Snake(self._canvas, 0, 0)
-        self.snake_list = [head, node2, node3]
+        self.head = Snake(self._canvas, 20, 0)
+        self.node2 = Snake(self._canvas, 10, 0)
+        self.node3 = Snake(self._canvas, 0, 0)
+        # self.snake_list = [head, node2, node3]
+        self.head.next = self.node2
+        self. node2.prev = self.head
+        self.node2.next = self.node3
+        self.node3.prev = self.node2
+        self.tail = self.node3
 
-        for snake in self.snake_list:
-            snake.snakeMove(speed=50, toX=1, toY=0)
+        t = threading.Timer(0.7, self.move)
+        t.start()
+
+        # for snake in self.snake_list:
+        #     snake.snakeMove(speed=50, toX=1, toY=0)
 
         # initial food
         self.food()
+
+    def move(self):
+        tmp = self.tail.prev
+        self.tail.moveTo(self.head.x + 10, self.head.y)
+        self.tail.prev = None
+        self.tail.next = self.head
+        self.head.prev = self.tail
+        self.head = self.tail
+        self.tail = tmp
+        self.tail.next = None
+
+        global t
+        t = threading.Timer(0.7, self.move)
+        t.start()
+
 
     def isEnd(self):
         return True
