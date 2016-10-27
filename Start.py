666c2f13ee9bfe6132f47e6352a870e91c538a5b
blueAnana <915935284@qq.com>
2016-10-27 23:57:12 +0800

    get key (umimplemented)

--- Start.py
+++ Start.py
@@ -1,12 +1,16 @@
 from Tkinter import *
 import random
 import threading
+import pyHook
+import pythoncom
+import msvcrt
 
 WIDTH = 500
 HEIGHT = 500
 BOUND_X = 10
 BOUND_Y = 10
 
+DIRECT = "Right"
 
 class Snake:
     def __init__(self, canvas):
@@ -28,7 +32,7 @@ class Snake:
         self.head = self.tail
         self.tail = tmp
         self.tail.next = None
-
+        print DIRECT
         global t
         if self.head.x < 470:
             t = threading.Timer(0.7, self.move)
@@ -88,10 +92,53 @@ class Start:
         self.y_food = random.randint(0, 48) * 10
         self.food = Node(canvas_=self._canvas, x_=self.x_food, y_=self.y_food)
 
+    # def getDirect(self):
+    #     while True:
+    #         key1 = msvcrt.getch()
+    #         print ord(key1)
+    #         if ord(key1) == '\xe0':
+    #             key2 = msvcrt.getch()
+    #             DIRECT = "Down"
+    #             print ord(key2)
+
+    def OnKeyboardEvent(event):
+        print 'MessageName:', event.MessageName
+        print 'Message:', event.Message
+        print 'Time:', event.Time
+        print 'Window:', event.Window
+        print 'WindowName:', event.WindowName
+        print 'Ascii:', event.Ascii, chr(event.Ascii)
+        print 'Key:', event.Key
+        print 'KeyID:', event.KeyID
+        print 'ScanCode:', event.ScanCode
+        print 'Extended:', event.Extended
+        print 'Injected:', event.Injected
+        print 'Alt', event.Alt
+        print 'Transition', event.Transition
+        print '---'
+
+        if event.Key == "Left" or event.Key == "Right" or event.Key == "Down" or event.Key == "Up":
+            DIRECT = event.Key
+
+        # return True to pass the event to other handlers
+        return True
+
+    def getDirect(self):
+        # create a hook manager
+        hm = pyHook.HookManager()
+        # watch for all mouse events
+        hm.KeyDown = self.OnKeyboardEvent
+        # set the hook
+        hm.HookKeyboard()
+        # wait forever
+        pythoncom.PumpMessages()
+
     def __init__(self, master=None):
         self._root = master
         self._canvas = Canvas(master, width=WIDTH, height=HEIGHT)
         self._canvas.pack()
+        t = threading.Timer(0.1, self.getDirect)
+        t.start()
         self.background()
 
 root = Tk()
